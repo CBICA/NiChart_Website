18dd59d701ce2c726c194dd1ddb946c1de539dd4
George Aidinis <gaidinis@live.com>
2023-06-26 17:43:22 -0400

    Added gradient header, lorem ipsum docs, floating header, sticky footer

--- pages/about.js
+++ pages/about.js
@@ -7,11 +7,13 @@ const About = () => {
     return (
       <div>
         <Header />
-        <div className={`${styles.container} ${styles.aboutContainer}`}>
-          <h1>About NiChart</h1>
-          <p>
-            NiChart (Neuro imaging Chart) is a software that provides a comprehensive solution for analyzing standard structural and functional brain MRI data across studies. It offers a range of features including computational morphometry, functional signal analysis, quality control, statistical harmonization, data standardization, interactive visualization, and extraction of expressive imaging signatures.
-          </p>
+        <div id="test">
+          <div className={`${styles.container} ${styles.aboutContainer}`}>
+            <h1>About NiChart</h1>
+            <p>
+              NiChart (Neuro imaging Chart) is a software that provides a comprehensive solution for analyzing standard structural and functional brain MRI data across studies. It offers a range of features including computational morphometry, functional signal analysis, quality control, statistical harmonization, data standardization, interactive visualization, and extraction of expressive imaging signatures.
+            </p>
+          </div>
         </div>
         <Footer />
       </div>
--- pages/documentation.js
+++ pages/documentation.js
@@ -1,15 +1,26 @@
-import React from 'react';
+import {React, useState, useEffect} from 'react';
 import Header from '../components/Layout/Header';
 import Footer from '../components/Layout/Footer';
 import styles from '../styles/Documentation.module.css';
+import ReactMarkdown from 'react-markdown';
 
 const Documentation = () => {
+    const [markdownContent, setMarkdownContent] = useState('');
+    
+    useEffect(() => {
+      fetch('/content/documentation-content.md')
+        .then((response) => response.text())
+        .then((content) => setMarkdownContent(content));
+    }, []);
+
     return (
       <div>
         <Header />
         <div className={styles.documentationPage}>
           <h2>Documentation</h2>
-          {/* Include the documentation content here */}
+          <div className="documentation-content">
+            <ReactMarkdown>{markdownContent}</ReactMarkdown>
+          </div>
         </div>
         <Footer />
       </div>

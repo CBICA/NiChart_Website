2ae99e7fbe78ff8ca1ec74fb52f9ea8330ad0295
George Aidinis <gaidinis@live.com>
2023-07-12 14:30:55 -0400

    Team page is now done, bug report template minor MD corrections

--- pages/team.js
+++ pages/team.js
@@ -1,26 +1,76 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Head from 'next/head';
 import Header from '../components/Layout/Header';
 import Footer from '../components/Layout/Footer';
 import styles from '../styles/Team.module.css';
+import Favicons from '../components/Favicons/Favicons';
+import teamMembers from '/public/content/Team/Team_Members.js'
+import collaborators from '/public/content/Team/Collaborators.js'
 
 const Team = () => {
-    return (
-      <div className={styles.container}>
-        <Header />
-        <div className={styles.team_page}>
-          <div className={styles.team_members}>
-            <h2>Team Members</h2>
-            {/* Include the team members content here */}
-          </div>
-          <div className={styles.team_collaborators}>
-            <h2>Collaborators</h2>
-            {/* Include the collaborators content here */}
+  const [teamExpandedIndex, setTeamExpandedIndex] = useState(-1);
+  const [collaboratorExpandedIndex, setCollaboratorExpandedIndex] = useState(-1);
+
+  const handleClick = (index, type) => {
+    if (type === 'team') {
+      if (teamExpandedIndex === index) {
+        setTeamExpandedIndex(-1);
+      } else {
+        setTeamExpandedIndex(index);
+      }
+    } else if (type === 'collaborator') {
+      if (collaboratorExpandedIndex === index) {
+        setCollaboratorExpandedIndex(-1);
+      } else {
+        setCollaboratorExpandedIndex(index);
+      }
+    }
+  };
+
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>NiChart | Team</title>
+        <Favicons />
+      </Head>
+      <Header />
+      <div className={styles.team_page}>
+        <div className={styles.team_members}>
+          <h2>Team Members</h2>
+          <div className={styles.grid}>
+            {teamMembers.map((member, index) => (
+              <div
+                key={index}
+                className={`${styles.member} ${teamExpandedIndex === index ? styles.expanded : ''}`}
+                onClick={() => handleClick(index, 'team')}
+              >
+                <img src={member.image} alt={member.name} />
+                <h3>{member.name}</h3>
+                {teamExpandedIndex === index && <p>{member.bio}</p>}
+              </div>
+            ))}
           </div>
         </div>
-        <Footer />
+        <div className={styles.team_members}>
+          <h2>Collaborators</h2>
+          <div className={styles.grid}>
+            {collaborators.map((collaborator, index) => (
+              <div
+                key={index}
+                className={`${styles.member} ${collaboratorExpandedIndex === index ? styles.expanded : ''}`}
+                onClick={() => handleClick(index, 'collaborator')}
+              >
+                <img src={collaborator.image} alt={collaborator.name} />
+                <h3>{collaborator.name}</h3>
+                {collaboratorExpandedIndex === index && <p>{collaborator.bio}</p>}
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
-    );
-  };
-  
-  export default Team;
-  
+      <Footer />
+    </div>
+  );
+};
+
+export default Team;
---
+++ public/content/Team/Collaborators.js
@@ -0,0 +1,95 @@
+const collaborators = [
+    {
+      name: 'Collaborator',
+      image: '/images/Team/user_stock_photo.png',
+      bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam varius felis a risus gravida sollicitudin. Phasellus quis nisi arcu.',
+    },
+    {
+      name: 'Collaborator',
+      image: '/images/Team/user_stock_photo.png',
+      bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam varius felis a risus gravida sollicitudin. Phasellus quis nisi arcu.',
+    },
+    {
+      name: 'Collaborator',
+      image: '/images/Team/user_stock_photo.png',
+      bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam varius felis a risus gravida sollicitudin. Phasellus quis nisi arcu.',
+    },
+    {
+      name: 'Collaborator',
+      image: '/images/Team/user_stock_photo.png',
+      bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam varius felis a risus gravida sollicitudin. Phasellus quis nisi arcu.',
+    },
+    {
+      name: 'Collaborator',
+      image: '/images/Team/user_stock_photo.png',
+      bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam varius felis a risus gravida sollicitudin. Phasellus quis nisi arcu.',
+    },
+    {
+      name: 'Collaborator',
+      image: '/images/Team/user_stock_photo.png',
+      bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam varius felis a risus gravida sollicitudin. Phasellus quis nisi arcu.',
+    },
+    {
+      name: 'Collaborator',
+      image: '/images/Team/user_stock_photo.png',
+      bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam varius felis a risus gravida sollicitudin. Phasellus quis nisi arcu.',
+    },
+    {
+      name: 'Collaborator',
+      image: '/images/Team/user_stock_photo.png',
+      bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam varius felis a risus gravida sollicitudin. Phasellus quis nisi arcu.',
+    },
+    {
+      name: 'Collaborator',
+      image: '/images/Team/user_stock_photo.png',
+      bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam varius felis a risus gravida sollicitudin. Phasellus quis nisi arcu.',
+    },
+    {
+      name: 'Collaborator',
+      image: '/images/Team/user_stock_photo.png',
+      bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam varius felis a risus gravida sollicitudin. Phasellus quis nisi arcu.',
+    },
+    {
+      name: 'Collaborator',
+      image: '/images/Team/user_stock_photo.png',
+      bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam varius felis a risus gravida sollicitudin. Phasellus quis nisi arcu.',
+    },
+    {
+      name: 'Collaborator',
+      image: '/images/Team/user_stock_photo.png',
+      bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam varius felis a risus gravida sollicitudin. Phasellus quis nisi arcu.',
+    },
+    {
+      name: 'Collaborator',
+      image: '/images/Team/user_stock_photo.png',
+      bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam varius felis a risus gravida sollicitudin. Phasellus quis nisi arcu.',
+    },
+    {
+      name: 'Collaborator',
+      image: '/images/Team/user_stock_photo.png',
+      bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam varius felis a risus gravida sollicitudin. Phasellus quis nisi arcu.',
+    },
+    {
+      name: 'Collaborator',
+      image: '/images/Team/user_stock_photo.png',
+      bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam varius felis a risus gravida sollicitudin. Phasellus quis nisi arcu.',
+    },
+    {
+      name: 'Collaborator',
+      image: '/images/Team/user_stock_photo.png',
+      bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam varius felis a risus gravida sollicitudin. Phasellus quis nisi arcu.',
+    },
+    {
+      name: 'Collaborator',
+      image: '/images/Team/user_stock_photo.png',
+      bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam varius felis a risus gravida sollicitudin. Phasellus quis nisi arcu.',
+    },
+    {
+      name: 'Collaborator',
+      image: '/images/Team/user_stock_photo.png',
+      bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam varius felis a risus gravida sollicitudin. Phasellus quis nisi arcu.',
+    },
+
+  ];
+
+  export default collaborators;

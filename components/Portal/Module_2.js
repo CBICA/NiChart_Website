77a63b152d98c5b8142cc9de61c473dccce053ba
George Aidinis <gaidinis@live.com>
2023-07-13 14:59:22 -0400

    Split portal into modules, added sidebar, some styling

---
+++ components/Portal/Module_2.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Module_2() {
+  return (
+    <div>
+      <h2>Module 2: ML</h2>
+      {/* Add the content and functionality for this module */}
+    </div>
+  );
+}
+
+export default Module_2;
---
+++ components/Portal/Module_3.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Module_3() {
+  return (
+    <div>
+      <h2>Module 3: Plotting</h2>
+      {/* Add the content and functionality for this module */}
+    </div>
+  );
+}
+
+export default Module_3;

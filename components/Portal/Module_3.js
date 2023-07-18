3c43a4bc474ac6cf29144de96effde817ebf3549
George Aidins <gaidinis@live.com>
2023-07-17 21:51:07 -0400

    init chart

--- components/Portal/Module_3.js
+++ components/Portal/Module_3.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Chart from './Chart';
 
 function Module_3() {
   return (
     <div>
       <h2>Module 3: Plotting</h2>
-      {/* Add the content and functionality for this module */}
+      <Chart />
     </div>
   );
 }

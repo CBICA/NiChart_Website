984a712921caaeedce1ab35c7493331380ef4199
George Aidinis <gaidinis@live.com>
2023-07-18 15:51:26 -0400

    Added layout for chart

--- components/Portal/Chart.js
+++ components/Portal/Chart.js
@@ -5,27 +5,42 @@ const PlotlyChart = dynamic(() => import('react-plotly.js'), { ssr: false });
 
 const Chart = () => {
   const [chartData, setChartData] = useState(null);
+  const [chartLayout, setChartLayout] = useState(null);
 
   useEffect(() => {
     // Fetch or generate your chart data here
     const data = [
       {
-        x: [1, 2, 3, 4, 5],
-        y: [1, 4, 9, 16, 25],
-        type: 'scatter',
-        mode: 'lines+markers',
-        marker: { color: 'red' },
+          type: 'scatter',  // all "scatter" attributes: https://plotly.com/javascript/reference/#scatter
+          x: [1, 2, 3],     // more about "x": #scatter-x
+          y: [3, 1, 6],     // #scatter-y
+          marker: {         // marker is an object, valid marker keys: #scatter-marker
+              color: 'rgb(16, 32, 77)' // more about "marker.color": #scatter-marker-color
+          }
       },
-    ];
+      {
+          type: 'bar',      // all "bar" chart attributes: #bar
+          x: [1, 2, 3],     // more about "x": #bar-x
+          y: [3, 1, 6],     // #bar-y
+          name: 'bar chart example' // #bar-name
+      }
+  ];
 
+  const layout = {             
+      title: 'simple example', 
+      xaxis: {                 
+          title: 'Age'         
+      }
+  };
     setChartData(data);
+    setChartLayout(layout);
   }, []);
 
   if (!chartData) {
     return <div>Loading chart...</div>;
   }
 
-  return <PlotlyChart data={chartData} layout={{ title: 'My Chart' }} />;
+  return <PlotlyChart data={chartData} layout={chartLayout} />;
 };
 
 export default Chart;

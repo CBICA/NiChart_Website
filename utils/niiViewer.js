1325dadafdeea618cac962e976218171b06be589
George Aidins <gaidinis@live.com>
2023-09-05 16:03:57 -0400

    Updated the NiiVue component to include overlays

--- utils/niiViewer.js
+++ utils/niiViewer.js
@@ -6,7 +6,21 @@ const NiiVue = ({ imageUrl }) => {
   useEffect(() => {
     const volumeList = [
       {
-        url: imageUrl,
+        // ORIGINAL
+        //url and urlImgData should be both specified if header and data are 
+        // different files
+        url: "/content/Portal/Visualization/Reference_Data/T1_0000.nii.gz",
+        colormap: "gray", // or "jet", "freesurfer"... https://niivue.github.io/niivue/features/colormaps.html
+        opacity: 1, // 0 to 1
+      },
+      {
+        // OVERLAY
+        url: "/content/Portal/Visualization/Reference_Data/T1_0000_muse_orig_orient.nii.gz",
+        colormap: "jet",
+        opacity: 0.5, // This is for the overlay.
+        cal_min: 47,
+        cal_max: 49,
+        // alphaThreshold: false
       },
     ];
     const nv = new Niivue({
@@ -21,3 +35,5 @@ const NiiVue = ({ imageUrl }) => {
 };
 
 export default NiiVue;
+
+

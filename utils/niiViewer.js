e38b40a2d84bbd7c9833c560d58129466e565eb0
George Aidins <gaidinis@live.com>
2023-09-08 16:16:06 -0400

    Checking if files exist, avoid site-breaking behaviour

--- utils/niiViewer.js
+++ utils/niiViewer.js
@@ -1,50 +1,86 @@
-import { useRef, useEffect } from "react";
+import { useRef, useEffect, useState } from "react";
 import { Niivue } from "@niivue/niivue";
-import MUSEROICompleteList from '/public/content/Portal/Visualization/Dicts/MUSE_ROI_complete_list.json';
 
 const NiiVue = ({ subjectID, roi }) => {
-  const canvas = useRef();
+  const canvas = useRef(null); // Initialize with null to avoid errors
+  const [originalFileExists, setOriginalFileExists] = useState(true); // Initialize as true
+  const [overlayExists, setOverlayExists] = useState(true); // Initialize as true
 
   useEffect(() => {
-    const volumeList = [];
-    // Add the original scan
-    volumeList.push({
-      url: `/content/Portal/Visualization/Subject_Scans/in/${subjectID}.nii.gz`,
-      // url: `/path/to/in/${subjectID}.nii.gz`,
-      colormap: "gray",
-      opacity: 1,
-    });
+    // Function to check if a file exists
+    const checkOriginalFileExists = async (url) => {
+      try {
+        const response = await fetch(url);
+        if (!response.ok) {
+          setOriginalFileExists(false); // File doesn't exist
+        }
+      } catch (error) {
+        setOriginalFileExists(false); // Error occurred, file doesn't exist
+      }
+    };
 
-    volumeList.push({
-      url: `/content/Portal/Visualization/Subject_Scans/out/${subjectID}_DLMUSE_${roi}.nii.gz`,
-      // url: `/path/to/out/out_muse_individual_roi_masks/${subjectID}_DLMUSE_${roiId}.nii.gz`,
-      colormap: "red",
-      opacity: 0.5,
-    });
-    // // Determine which ROIs should be added based on MUSEROICompleteList
-    // const roiData = MUSEROICompleteList[roi];
-    // if (roiData) {
-    //   const roiIds = roiData.Consisting_of_ROIS.split(", ");
-    //   roiIds.forEach((roiId) => {
-    //     volumeList.push({
-    //       url: `/content/Portal/Visualization/Subject_Scans/out/${subjectID}_DLMUSE_${roiId}.nii.gz`,
-    //       // url: `/path/to/out/out_muse_individual_roi_masks/${subjectID}_DLMUSE_${roiId}.nii.gz`,
-    //       colormap: "red",
-    //       opacity: 0.5,
-    //     });
-    //   });
-    // }
+    const checkOverlayExists = async (url) => {
+      try {
+        const response = await fetch(url);
+        if (!response.ok) {
+          setOverlayExists(false); // File doesn't exist
+        }
+      } catch (error) {
+        setOverlayExists(false); // Error occurred, file doesn't exist
+      }
+    };
 
-    const nv = new Niivue({
-      isColorbar: false,
-      show3Dcrosshair: true,
-    });
+    // Check if original scan exists
+    const originalScanURL = `/content/Portal/Visualization/Subject_Scans/in/${subjectID}.nii.gz`;
+    checkOriginalFileExists(originalScanURL);
 
-    nv.attachToCanvas(canvas.current);
-    nv.loadVolumes(volumeList);
+    // Check if overlay exists
+    const overlayURL = `/content/Portal/Visualization/Subject_Scans/out/${subjectID}_DLMUSE_${roi}.nii.gz`;
+    checkOverlayExists(overlayURL);
   }, [subjectID, roi]);
 
-  return <canvas ref={canvas} height={480} width={640} />;
+  // Conditionally render the error message or canvas
+  if (!originalFileExists) {
+    // Display an error message if the original file does not exist
+    return <p>Error: Original scan does not exist.</p>;
+  }
+
+  const volumeList = [];
+  // Add the original scan
+  const originalScan = {
+    url: `/content/Portal/Visualization/Subject_Scans/in/${subjectID}.nii.gz`,
+    colormap: "gray",
+    opacity: 1,
+  };
+  volumeList.push(originalScan);
+
+  // Add the overlay, if it exists
+  if (overlayExists) {
+    const overlay = {
+      url: `/content/Portal/Visualization/Subject_Scans/out/${subjectID}_DLMUSE_${roi}.nii.gz`,
+      colormap: "red",
+      opacity: 0.5,
+    };
+    volumeList.push(overlay);
+  }
+
+  const nv = new Niivue({
+    isColorbar: false,
+    show3Dcrosshair: true,
+  });
+
+  // Ensure canvas.current is defined before attaching
+  if (canvas.current) {
+    nv.attachToCanvas(canvas.current);
+    nv.loadVolumes(volumeList);
+  }
+
+  return (
+    <>
+      {!originalFileExists && <p>Error: Original scan does not exist.</p>}
+      <canvas ref={canvas} height={480} width={640} />
+    </>
+  );
 };
 
 export default NiiVue;

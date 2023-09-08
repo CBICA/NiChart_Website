df9b1c1d14cbacfc455f7a6fb9b34c6d57cabba0
George Aidins <gaidinis@live.com>
2023-09-08 16:34:04 -0400

    Fixed one case where if the file didn't exist, the error would still break the site

--- utils/niiViewer.js
+++ utils/niiViewer.js
@@ -2,83 +2,66 @@ import { useRef, useEffect, useState } from "react";
 import { Niivue } from "@niivue/niivue";
 
 const NiiVue = ({ subjectID, roi }) => {
-  const canvas = useRef(null); // Initialize with null to avoid errors
-  const [originalFileExists, setOriginalFileExists] = useState(true); // Initialize as true
-  const [overlayExists, setOverlayExists] = useState(true); // Initialize as true
+  const canvas = useRef(null);
+  const [isError, setIsError] = useState(false);
 
   useEffect(() => {
-    // Function to check if a file exists
-    const checkOriginalFileExists = async (url) => {
+    const checkFiles = async () => {
+      const originalScanURL = `/content/Portal/Visualization/Subject_Scans/in/${subjectID}.nii.gz`;
+      const overlayURL = `/content/Portal/Visualization/Subject_Scans/out/${subjectID}_DLMUSE_${roi}.nii.gz`;
+
       try {
-        const response = await fetch(url);
-        if (!response.ok) {
-          setOriginalFileExists(false); // File doesn't exist
+        // Check if the original scan file exists
+        const originalResponse = await fetch(originalScanURL);
+        if (!originalResponse.ok) {
+          throw new Error("Original scan does not exist.");
+        }
+
+        // Check if the overlay file exists
+        const overlayResponse = await fetch(overlayURL);
+        if (!overlayResponse.ok) {
+          throw new Error("Overlay does not exist.");
+        }
+
+        // If both files exist, proceed with visualization
+        const volumeList = [
+          {
+            url: originalScanURL,
+            colormap: "gray",
+            opacity: 1,
+          },
+          {
+            url: overlayURL,
+            colormap: "red",
+            opacity: 0.5,
+          },
+        ];
+
+        const nv = new Niivue({
+          isColorbar: false,
+          show3Dcrosshair: true,
+        });
+
+        if (canvas.current) {
+          nv.attachToCanvas(canvas.current);
+          nv.loadVolumes(volumeList);
         }
       } catch (error) {
-        setOriginalFileExists(false); // Error occurred, file doesn't exist
+        console.error(error.message);
+        setIsError(true);
       }
     };
 
-    const checkOverlayExists = async (url) => {
-      try {
-        const response = await fetch(url);
-        if (!response.ok) {
-          setOverlayExists(false); // File doesn't exist
-        }
-      } catch (error) {
-        setOverlayExists(false); // Error occurred, file doesn't exist
-      }
-    };
-
-    // Check if original scan exists
-    const originalScanURL = `/content/Portal/Visualization/Subject_Scans/in/${subjectID}.nii.gz`;
-    checkOriginalFileExists(originalScanURL);
-
-    // Check if overlay exists
-    const overlayURL = `/content/Portal/Visualization/Subject_Scans/out/${subjectID}_DLMUSE_${roi}.nii.gz`;
-    checkOverlayExists(overlayURL);
+    checkFiles();
   }, [subjectID, roi]);
-
-  // Conditionally render the error message or canvas
-  if (!originalFileExists) {
-    // Display an error message if the original file does not exist
-    return <p>Error: Original scan does not exist.</p>;
-  }
-
-  const volumeList = [];
-  // Add the original scan
-  const originalScan = {
-    url: `/content/Portal/Visualization/Subject_Scans/in/${subjectID}.nii.gz`,
-    colormap: "gray",
-    opacity: 1,
-  };
-  volumeList.push(originalScan);
-
-  // Add the overlay, if it exists
-  if (overlayExists) {
-    const overlay = {
-      url: `/content/Portal/Visualization/Subject_Scans/out/${subjectID}_DLMUSE_${roi}.nii.gz`,
-      colormap: "red",
-      opacity: 0.5,
-    };
-    volumeList.push(overlay);
-  }
-
-  const nv = new Niivue({
-    isColorbar: false,
-    show3Dcrosshair: true,
-  });
-
-  // Ensure canvas.current is defined before attaching
-  if (canvas.current) {
-    nv.attachToCanvas(canvas.current);
-    nv.loadVolumes(volumeList);
-  }
 
   return (
     <>
-      {!originalFileExists && <p>Error: Original scan does not exist.</p>}
-      <canvas ref={canvas} height={480} width={640} />
+      {isError ? (
+        <p>Error: One or more files do not exist.</p>
+      ) : (
+        <canvas ref={canvas} height={480} width={640} />
+      )}
     </>
   );
 };

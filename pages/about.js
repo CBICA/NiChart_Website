08478dd09d335722788ca606a4acf9d72164d6a8
George Aidins <gaidinis@live.com>
2023-09-28 16:41:07 -0400

    Updated content & look of About page, improved navigation between Image Processing components and About

--- pages/about.js
+++ pages/about.js
@@ -2,57 +2,90 @@ import React from 'react';
 import Head from 'next/head';
 import Header from '../components/Layout/Header';
 import Footer from '../components/Layout/Footer';
-import styles from '../styles/About.module.css';
 import Favicons from '../components/Favicons/Favicons';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+import Divider from '@mui/material/Divider';
+import styles from '../styles/About.module.css';
+
+const contentData = [
+  {
+    text:
+      "NiChart is an ecosystem of software components enabling constructive integration, statistical harmonization, and ML-centric data analyses across studies.",
+    image: "/images/About/NiChart_overview_diagram.svg",
+  },
+  {
+    text:
+      "Key to NiChart's essence is its role as a versatile machine learning research platform. Users can harness the capabilities of NiChart to develop and validate their own machine learning models and pipelines.",
+    image: "/images/About/NiChart_Data_Interaction.svg",
+  },
+  {
+    text:
+      "NiChart accommodates a range of brain scan types, including sMRI, fMRI, and DTI. This flexibility provides researchers with ample opportunities to explore various imaging modalities.",
+    image: "/images/About/NiChart_Modalities.png",
+  },
+  {
+    text:
+      "NiChart is built upon a substantial dataset of 65,693 individuals, comprising 84,211 time points. This data has undergone meticulous curation, harmonization, and quality control.",
+    image: "/images/About/NiChart_overview_diagram.svg",
+  },
+  {
+    text:
+      "NiChart automates the labor-intensive aspects of data processing, facilitating feature extraction and ML model application. This efficiency significantly reduces the time required for data analysis.",
+    image: "/images/About/NiChart_Data_Interaction.svg",
+  },
+];
+
+const replaceWithLinks = (text) => {
+  // Use regular expressions to replace the words with links
+  return text.replace(/sMRI/g, '<a href="/documentation/?section=sMRI">sMRI</a>')
+             .replace(/fMRI/g, '<a href="/documentation/?section=fMRI">fMRI</a>')
+             .replace(/DTI/g, '<a href="/documentation/?section=DTI">DTI</a>');
+};
 
 const About = () => {
-    return (
-      <div className={styles.container}>
-        <Head>
-            <title>NiChart | About</title>
-            <Favicons />
-        </Head>
-        <Header />
-        <div className={styles.container}>
-          <div className={styles.title}>
-              <h1>About NiChart</h1>
-          </div>
-          <div className={styles.aboutContent}>
-              <div className={styles.aboutHeaderPicture}>
-                  <img src="/images/About/about_banner_picture_2.jpg" alt="NiChart Logo - Image by Gerd Altmann from Pixabay"/>
-              </div>
-              <div className={styles.aboutMainContent}>
-                  {/* General overview */}
-                  <p>NiChart is an ecosystem of software components enabling constructive integration, statistical harmonization, and ML-centric data analyses across studies. </p>
-                  <img src="/images/About/NiChart_overview_diagram.svg"  style={{ paddingLeft: '5%', paddingRight: '5%', maxWidth: '80%', height: 'auto' }} alt="Overview Diagram"/>
-                  
-                  {/* Model training capability emphasis */}
-                  <p>Key to NiChart's essence is its role as a versatile machine learning research platform. Users can harness the capabilities of NiChart to develop and validate their own machine learning models and pipelines.</p>
-                  {/* <img src="/images/About/NiChart_overview_diagram.svg"  style={{ paddingLeft: '5%', paddingRight: '5%', maxWidth: '80%', height: 'auto' }} alt="Overview Diagram"/> */}
-                  
-                  {/* Different modalities */}
-                  <p>
-                    NiChart accommodates a range of brain scan types, including{" "}
-                    <a href="/documentation/#sMRI">sMRI</a>,{" "}
-                    <a href="/documentation/#fMRI">fMRI</a>, and{" "}
-                    <a href="/documentation/#DTI">DTI</a>. This flexibility provides researchers with ample opportunities to explore various imaging modalities.
-                  </p>
-                  {/* <img src="/images/About/NiChart_overview_diagram.svg"  style={{ paddingLeft: '5%', paddingRight: '5%', maxWidth: '80%', height: 'auto' }} alt="Overview Diagram"/> */}
-                  
-                  {/* Reference data */}
-                  <p>NiChart is built upon a substantial dataset of 65,693 individuals, comprising 84,211 time points. This data has undergone meticulous curation, harmonization, and quality control.</p>
-                  {/* <img src="/images/About/NiChart_overview_diagram.svg"  style={{ paddingLeft: '5%', paddingRight: '5%', maxWidth: '80%', height: 'auto' }} alt="Overview Diagram"/> */}
-                  
-                  {/* Efficiency */}
-                  <p>NiChart automates the labor-intensive aspects of data processing, facilitating feature extraction and ML model application. This efficiency significantly reduces the time required for data analysis.</p>
-                  {/* <img src="/images/About/NiChart_overview_diagram.svg"  style={{ paddingLeft: '5%', paddingRight: '5%', maxWidth: '80%', height: 'auto' }} alt="Overview Diagram"/> */}
-              </div>
-          </div>
-          
-        </div>
-        <Footer />
+  return (
+    <div>
+      <Head>
+        <title>NiChart | About</title>
+        <Favicons />
+      </Head>
+      <Header />
+      <div>
+        <Container maxWidth="x-lg">
+          <Typography variant="h4" align="center" className={styles.title}>
+            About NiChart
+          </Typography>
+          {contentData.map((item, index) => (
+            <div key={index}>
+              <Paper elevation={3} className={styles.row}>
+                <Grid container spacing={2} alignItems="center">
+                  <Grid item xs={12} sm={6}>
+                    <Typography variant="body1" className={styles.text}>
+                      {/* Use dangerouslySetInnerHTML to render HTML */}
+                      <span dangerouslySetInnerHTML={{ __html: replaceWithLinks(item.text) }} />
+                    </Typography>
+                  </Grid>
+                  <Grid item xs={12} sm={6}>
+                    <img
+                      src={item.image}
+                      alt={`Diagram ${index}`}
+                      className={styles.image}
+                    />
+                  </Grid>
+                </Grid>
+              </Paper>
+              <Divider className={styles.divider} />
+            </div>
+          ))}
+        </Container>
       </div>
-    );
-  };
-  
-  export default About;
+      <Footer />
+    </div>
+  );
+};
+
+export default About;

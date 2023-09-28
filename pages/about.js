162d0e1d0f2d13fffa4656e7b3ac8615de9feea6
George Aidins <gaidinis@live.com>
2023-09-28 15:01:23 -0400

    Linked <p> from about to Image Processing Components

--- pages/about.js
+++ pages/about.js
@@ -22,16 +22,30 @@ const About = () => {
                   <img src="/images/About/about_banner_picture_2.jpg" alt="NiChart Logo - Image by Gerd Altmann from Pixabay"/>
               </div>
               <div className={styles.aboutMainContent}>
+                  {/* General overview */}
                   <p>NiChart is an ecosystem of software components enabling constructive integration, statistical harmonization, and ML-centric data analyses across studies. </p>
                   <img src="/images/About/NiChart_overview_diagram.svg"  style={{ paddingLeft: '5%', paddingRight: '5%', maxWidth: '80%', height: 'auto' }} alt="Overview Diagram"/>
-                  <p>NiChart enables large-scale analyses of multi-modal brain MRI data as well as mapping of such data into a dimensional system of neuroimaging signatures implemented by a large number of ML models. The axes of this dimensional system represent two types of information:</p>
-                  <ol>
-                  <li>a panel of structural (sMRI), diffusion (dMRI) and functional connectomic (fcMRI) imaging derived phenotypes, such as multi-scale brain parcellations and brain networks;</li>
-                  <li>complex ML-based imaging signatures previously derived from carefully processed and curated data of 65,000 individuals from 22 harmonized studies. ML signatures capture multi-variate imaging patterns that reflect the heterogeneity of brain aging, neurodegeneration, as well as of neuropsychiatic disorders</li>
-                  </ol>
-                  <p>Using our software toolboxes researchers will be able to map new data into NiChart, ML models trained in NiChart, compare their data with NiChart-based normative ranges, and jointly evaluate their results vis-a-vis those of other studies. </p>
-                  <p>Users will also be able to contribute their own ML models back to NiChart, thereby contributing to a dynamically growing community using ML neuroimaging models.</p>
-                  <p>Software deployment accommodates different case scenarios, providing python installation packages through PIP, reproducible Docker and Singularity containers and a web application on AWS.</p>
+                  
+                  {/* Model training capability emphasis */}
+                  <p>Key to NiChart's essence is its role as a versatile machine learning research platform. Users can harness the capabilities of NiChart to develop and validate their own machine learning models and pipelines.</p>
+                  {/* <img src="/images/About/NiChart_overview_diagram.svg"  style={{ paddingLeft: '5%', paddingRight: '5%', maxWidth: '80%', height: 'auto' }} alt="Overview Diagram"/> */}
+                  
+                  {/* Different modalities */}
+                  <p>
+                    NiChart accommodates a range of brain scan types, including{" "}
+                    <a href="/documentation/#sMRI">sMRI</a>,{" "}
+                    <a href="/documentation/#fMRI">fMRI</a>, and{" "}
+                    <a href="/documentation/#DTI">DTI</a>. This flexibility provides researchers with ample opportunities to explore various imaging modalities.
+                  </p>
+                  {/* <img src="/images/About/NiChart_overview_diagram.svg"  style={{ paddingLeft: '5%', paddingRight: '5%', maxWidth: '80%', height: 'auto' }} alt="Overview Diagram"/> */}
+                  
+                  {/* Reference data */}
+                  <p>NiChart is built upon a substantial dataset of 65,693 individuals, comprising 84,211 time points. This data has undergone meticulous curation, harmonization, and quality control.</p>
+                  {/* <img src="/images/About/NiChart_overview_diagram.svg"  style={{ paddingLeft: '5%', paddingRight: '5%', maxWidth: '80%', height: 'auto' }} alt="Overview Diagram"/> */}
+                  
+                  {/* Efficiency */}
+                  <p>NiChart automates the labor-intensive aspects of data processing, facilitating feature extraction and ML model application. This efficiency significantly reduces the time required for data analysis.</p>
+                  {/* <img src="/images/About/NiChart_overview_diagram.svg"  style={{ paddingLeft: '5%', paddingRight: '5%', maxWidth: '80%', height: 'auto' }} alt="Overview Diagram"/> */}
               </div>
           </div>
           

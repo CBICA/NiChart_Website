718c0f251eca0c53c1f10d1469417478acef3dfa
gurayerus <gurayerus@gmail.com>
2023-11-20 21:13:26 -0500

    Add About page content

--- pages/about.js
+++ pages/about.js
@@ -54,6 +54,29 @@ const About = () => {
         <Favicons />
       </Head>
       <Header />
+
+        <h2>Overview</h2>
+        Rapidly growing neuroimaging data that was acquired over decades in multiple studies, in conjunction with the advent of advanced machine learning methods, has created 
+        tremendous potential for <font color="#aa00dd">knowledge discovery at a large scale</font>. NiChart (NeuroImaging Chart) is an ecosystem of software components enabling <font color="#aa00dd">constructive integration, statistical harmonization, and ML-centric data analyses</font> across studies.
+        
+        <h2>Applications</h2>
+        NiChart enables mapping of large-scale multi-modal brain MRI data into a dimensional system of neuroimaging signatures implemented by our ML models. 
+        
+        <ul>
+        <li>
+        We provide image processing tools for deriving <font color="#aa00dd">a panel of imaging derived phenotypes</font> from structural (sMRI), diffusion (dMRI) and functional (fMRI) imaging data.
+        </li>
+        <li>
+        ML models, which are previously trained on carefully processed and curated reference samples, allow users to calculate <font color="#aa00dd">imaging signatures that quantify complex multi-variate imaging patterns of brain changes</font> mapping the image data into a small but informative set of neuroimaging chart dimensions.
+        </li>
+        <li>
+        ML models capture heterogeneity of brain aging and neurodegeneration, and atrophy patterns due to <font color="#aa00dd">various diseases and conditions</font>, such as Alzheimer's disease, neuropsychiatic disorders, or cardio-vascular risk factors. 
+        </li>
+        <li>
+        Standardized values from the large reference set allow users to compare their data with <font color="#aa00dd">NiChart-based normative ranges</font> or distributions from <font color="#aa00dd">specific disease subgroups</font>.
+        </li>
+        </ul>
+        
       <div className={styles.container}>
         {/* <Container maxWidth="x-lg">
           <Typography variant="h4" align="center" className={styles.title}>
@@ -81,7 +104,8 @@ const About = () => {
             </div>
           ))}
         </Container> */}
-        About content goes here
+
+        
       </div>
       <Footer />
     </div>

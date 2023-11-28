f32ab0a8cdd66aa803d8c43ed81f0e2f37c4db6b
George Aidins <gaidinis@live.com>
2023-11-28 17:41:59 +0000

    Template modal added

---
+++ components/Components/Modal.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import Modal from '@mui/material/Modal';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  boxShadow: 24,
+  p: 4,
+};
+
+const Modal = ({ open, handleClose, title, content }) => {
+  return (
+    <Modal
+      open={open}
+      onClose={handleClose}
+      aria-labelledby="modal-title"
+      aria-describedby="modal-description"
+    >
+      <Box sx={style}>
+        <Typography id="modal-title" variant="h6" component="h2">
+          {title}
+        </Typography>
+        <Typography id="modal-description" sx={{ mt: 2 }}>
+          {content}
+        </Typography>
+        <Button onClick={handleClose}>Close</Button>
+      </Box>
+    </Modal>
+  );
+};
+
+export default Modal;
+

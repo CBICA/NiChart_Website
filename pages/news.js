cde55c31af903a856967a06a0f37f83ad6cbf712
George Aidins <gaidinis@live.com>
2023-12-18 10:22:59 -0500

    Updated News, fixed a link in About, removed styling from Publications

--- pages/news.js
+++ pages/news.js
@@ -10,51 +10,36 @@ import Link from 'next/link';
 const News = () => {
   const newsArticles = [
     {
-      title: "NiChart Collaborates with Major University",
-      date: "March 15, 2023",
-      summary: "NiChart has entered into a collaboration with XYZ University to enhance MRI imaging analysis...",
-      link: "/news/collaboration-xyz-university"
+      title: "NiChart v1.0 is released!",
+      date: "December 22, 2023",
+      summary: "You can now use NiChart to process your data!",
+      link: "https://twitter.com/NiChart_AIBIL"
     },
-    {
-      title: "New Breakthrough in MRI Imaging",
-      date: "April 22, 2023",
-      summary: "A groundbreaking study using NiChart has led to new insights in MRI imaging techniques...",
-      link: "/news/mri-breakthrough"
-    },
-    {
-      title: "NiChart at the Global Health Conference",
-      date: "May 9, 2023",
-      summary: "NiChart will be featured at the upcoming Global Health Conference, showcasing its latest advancements...",
-      link: "/news/global-health-conference"
-    }
   ];
   return (
     <div className={styles.container}>
       <Head>
         <title>NiChart | News</title>
         <Favicons />
+        <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
       </Head>
       <Header />
-      
-      <h1>News</h1>
-      <div className={styles.newsLayout}>
-        <div className={styles.newsArticles}>
-          {newsArticles.map((article, index) => (
-            <div key={index} className={styles.article}>
-              <h2>{article.title}</h2>
-              <p className={styles.date}>{article.date}</p>
-              <p>{article.summary}</p>
-              <Link href={article.link}><a>Read more</a></Link>
-            </div>
-          ))}
-        </div>
-
-        <div className={styles.twitterFeed}>
-          <a 
-            className="twitter-timeline" 
-            href="https://twitter.com/NiChart_AIBIL?ref_src=twsrc%5Etfw">Tweets by NiChart_AIBIL
-          </a> 
-          <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+      <div className={styles.mainContent}>
+        <h1>News</h1>
+        <div className={styles.newsLayout}>
+          <div className={styles.newsArticles}>
+            {newsArticles.map((article, index) => (
+              <div key={index} className={styles.article}>
+                <h2>{article.title}</h2>
+                <p className={styles.date}>{article.date}</p>
+                <p>{article.summary}</p>
+                <Link href={article.link}><a>Read more</a></Link>
+              </div>
+            ))}
+          </div>
+          <div className={styles.twitterFeed}>
+            <a class="twitter-timeline" href="https://twitter.com/NiChart_AIBIL?ref_src=twsrc%5Etfw">Tweets by NiChart_AIBIL</a>
+          </div>
         </div>
       </div>
       <Footer />
